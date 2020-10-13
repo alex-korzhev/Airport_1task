7a2c796e473ff9534ee435f28f2416c2967b5de4
Alex Korzhev <nfe.unsert@gmail.com>
2020-10-13 23:06:17 +0800

    Completed 2nd task

--- Airport_1task/Form1.Designer.cs
+++ Airport_1task/Form1.Designer.cs
@@ -30,10 +30,11 @@
         {
             this.btn_open_file = new System.Windows.Forms.Button();
             this.lv_opened_files = new System.Windows.Forms.ListView();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.tb_main_text = new System.Windows.Forms.TextBox();
-            this.lbl_opened_files = new System.Windows.Forms.Label();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_load_files = new System.Windows.Forms.Button();
+            this.lbl_opened_files = new System.Windows.Forms.Label();
+            this.tb_main_text = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -51,18 +52,24 @@
             // 
             this.lv_opened_files.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader1});
+            this.lv_opened_files.FullRowSelect = true;
             this.lv_opened_files.HideSelection = false;
             this.lv_opened_files.Location = new System.Drawing.Point(0, 79);
             this.lv_opened_files.MultiSelect = false;
             this.lv_opened_files.Name = "lv_opened_files";
-            this.lv_opened_files.Size = new System.Drawing.Size(207, 217);
+            this.lv_opened_files.Size = new System.Drawing.Size(207, 173);
             this.lv_opened_files.TabIndex = 1;
             this.lv_opened_files.UseCompatibleStateImageBehavior = false;
             this.lv_opened_files.View = System.Windows.Forms.View.List;
             this.lv_opened_files.Click += new System.EventHandler(this.lv_opened_files_Click);
             // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Width = -2;
+            // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.btn_load_files);
             this.panel1.Controls.Add(this.lbl_opened_files);
             this.panel1.Controls.Add(this.btn_open_file);
             this.panel1.Controls.Add(this.lv_opened_files);
@@ -71,14 +78,15 @@
             this.panel1.Size = new System.Drawing.Size(207, 296);
             this.panel1.TabIndex = 2;
             // 
-            // tb_main_text
+            // btn_load_files
             // 
-            this.tb_main_text.Location = new System.Drawing.Point(226, 12);
-            this.tb_main_text.Multiline = true;
-            this.tb_main_text.Name = "tb_main_text";
-            this.tb_main_text.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.tb_main_text.Size = new System.Drawing.Size(509, 296);
-            this.tb_main_text.TabIndex = 3;
+            this.btn_load_files.Location = new System.Drawing.Point(0, 254);
+            this.btn_load_files.Name = "btn_load_files";
+            this.btn_load_files.Size = new System.Drawing.Size(207, 38);
+            this.btn_load_files.TabIndex = 3;
+            this.btn_load_files.Text = "Load Files";
+            this.btn_load_files.UseVisualStyleBackColor = true;
+            this.btn_load_files.Click += new System.EventHandler(this.btn_load_files_Click);
             // 
             // lbl_opened_files
             // 
@@ -92,6 +100,15 @@
             this.lbl_opened_files.Text = "Opened Files List:";
             this.lbl_opened_files.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // tb_main_text
+            // 
+            this.tb_main_text.Location = new System.Drawing.Point(226, 12);
+            this.tb_main_text.Multiline = true;
+            this.tb_main_text.Name = "tb_main_text";
+            this.tb_main_text.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tb_main_text.Size = new System.Drawing.Size(509, 296);
+            this.tb_main_text.TabIndex = 3;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -101,6 +118,7 @@
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -115,6 +133,7 @@
         private System.Windows.Forms.TextBox tb_main_text;
         private System.Windows.Forms.Label lbl_opened_files;
         private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.Button btn_load_files;
     }
 }
 
--- Airport_1task/Form1.cs
+++ Airport_1task/Form1.cs
@@ -1,69 +1,136 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Serialization;
+using System.Xml;
 
 namespace Airport_1task
 {
     public partial class Form1 : Form
     {
 
-        List<TextFile> textFiles;
-        private OpenFileDialog ofd;
-
+        public FileHandler fh;
         public Form1()
         {
             InitializeComponent();
-            textFiles = new List<TextFile>();
+            lv_opened_files.Columns[0].Width = lv_opened_files.Width;
+            fh = new FileHandler(lv_opened_files);
         }
 
         private void btn_open_file_Click(object sender, EventArgs e)
         {
-            TextFile tf = openFile();
-            if (tf != null)
+            TextFile tf = fh.LoadFile();
+            fh.showFile(tf, tb_main_text);
+            fh.selectFile(tf);
+        }
+
+        private void lv_opened_files_Click(object sender, EventArgs e)
+        {
+            try
             {
-                addTFToForm(tf);
+                ListViewItem selectedFile = lv_opened_files.SelectedItems[0];
+                fh.showFile(fh.FindTFByPath(selectedFile.Tag.ToString()), tb_main_text);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+
             }
         }
 
-        private void addTFToForm(TextFile tf)
+        private void btn_load_files_Click(object sender, EventArgs e)
         {
-            textFiles.Add(tf);
-            tb_main_text.Text = tf.Text;
-            ListViewItem lvi = new ListViewItem();
-            lvi.Text = tf.Name;
-            lvi.Tag = textFiles.IndexOf(tf);
-            lv_opened_files.Items.Add(lvi);
+            fh.LoadFileList();
         }
 
-        private TextFile openFile()
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            fh.SaveFileList();
+        }
+    }
+
+    public class FileHandler
+    {
+        readonly string fileListPath = Application.StartupPath + "\\filelist.xml";
+        private OpenFileDialog ofd;
+
+        public List<TextFile> TextFiles { get; set; }
+        public ListView SyncedLV { get; set; }
+
+        public FileHandler(ListView lv)
+        {
+            TextFiles = new List<TextFile>();
+            SyncedLV = lv;
+        }
+
+        public void RedrawListView()
+        {
+            SyncedLV.Clear();
+            foreach (TextFile tf in TextFiles)
+            {
+                AddToListView(tf);
+            }
+        }
+
+        public TextFile FindTFByPath(String path)
+        {
+            return TextFiles.Find(tf => tf.Path == path);
+        }
+
+        public void AddToListView(TextFile tf)
+        {
+            ListViewItem lvi = new ListViewItem();
+            lvi.Text = tf.Name;
+            lvi.Tag = tf.Path;
+            SyncedLV.Items.Add(lvi);
+        }
+
+        public void selectFile(TextFile tf)
+        {
+            if (tf != null)
+            {
+                SyncedLV.Items[TextFiles.IndexOf(tf)].Selected = true;
+                SyncedLV.Select();
+            }
+        }
+
+        public void showFile(TextFile tf, TextBox tb)
+        {
+            if (tf != null)
+            {
+                tb.Text = tf.Text;
+            }
+            else
+            {
+                tb.Clear();
+            }
+        }
+
+        public TextFile LoadFile()
         {
             ofd = new OpenFileDialog();
+            ofd.Filter = "txt files (*.txt)|*.txt";
+            ofd.FilterIndex = 1;
             if (ofd.ShowDialog() == DialogResult.OK)
             {
-                TextFile temptf = textFiles.Find(tf => tf.Path == ofd.FileName);
+                TextFile temptf = TextFiles.Find(tf => tf.Path == ofd.FileName);
                 if (temptf != null)
                 {
-                    showFile(temptf);
-                    return null;
+                    return temptf;
                 }
                 else
                 {
                     try
                     {
                         var sr = new StreamReader(ofd.FileName);
-                        return new TextFile(ofd.SafeFileName, sr.ReadToEnd(), ofd.FileName);
-
+                        TextFiles.Add(new TextFile(ofd.SafeFileName, sr.ReadToEnd(), ofd.FileName));
+                        AddToListView(TextFiles.Last());
+                        return TextFiles.Last();
                     }
                     catch (Exception ex)
                     {
-                        MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
-                        $"Details:\n\n{ex.StackTrace}");
+                        MessageBox.Show("Ошибка при открытии файла: \r\n" + ex.StackTrace);
                         return null;
                     }
                 }
@@ -74,26 +141,41 @@ namespace Airport_1task
             }
         }
 
-        private void lv_opened_files_Click(object sender, EventArgs e)
+        public void LoadFileList()
         {
-            ListViewItem selectedFile = lv_opened_files.SelectedItems[0];
-            showFile(textFiles[Int32.Parse(selectedFile.Tag.ToString())]);
+            if (File.Exists(fileListPath))
+            {
+                XmlSerializer reader = new XmlSerializer(TextFiles.GetType());
+                StreamReader file = new StreamReader(fileListPath);
+                TextFiles = new List<TextFile>();
+                TextFiles = (List<TextFile>)reader.Deserialize(file);
+                file.Close();
+                RedrawListView();
+            }
         }
 
-        private void showFile(TextFile tf)
+        public void SaveFileList()
         {
-            tb_main_text.Text = tf.Text;
-            lv_opened_files.Items[textFiles.IndexOf(tf)].Selected = true;
-            lv_opened_files.Select();
+            XmlWriterSettings ws = new XmlWriterSettings();
+            ws.NewLineHandling = NewLineHandling.Entitize;
+            XmlSerializer ser = new XmlSerializer(TextFiles.GetType());
+            using (XmlWriter wr = XmlWriter.Create(fileListPath, ws))
+            {
+                ser.Serialize(wr, TextFiles);
+            }
         }
+
     }
-
-    public class TextFile
+        public class TextFile
     {
         public string Name { get; set; }
         public string Text { get; set; }
         public string Path { get; set; }
 
+        public TextFile()
+        {
+
+        }
         public TextFile(string name, string text, string path)
         {
             Name = name;
