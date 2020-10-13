2edc7009b59b7219b93aa65cd2478edd648c02bc
Alex Korzhev <nfe.unsert@gmail.com>
2020-10-13 20:12:24 +0800

    Partially completed the 1st part

--- Airport_1task/Form1.Designer.cs
+++ Airport_1task/Form1.Designer.cs
@@ -28,13 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_open_file = new System.Windows.Forms.Button();
+            this.lv_opened_files = new System.Windows.Forms.ListView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tb_main_text = new System.Windows.Forms.TextBox();
+            this.lbl_opened_files = new System.Windows.Forms.Label();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_open_file
+            // 
+            this.btn_open_file.Location = new System.Drawing.Point(0, 0);
+            this.btn_open_file.Name = "btn_open_file";
+            this.btn_open_file.Size = new System.Drawing.Size(207, 50);
+            this.btn_open_file.TabIndex = 0;
+            this.btn_open_file.Text = "Open File";
+            this.btn_open_file.UseVisualStyleBackColor = true;
+            this.btn_open_file.Click += new System.EventHandler(this.btn_open_file_Click);
+            // 
+            // lv_opened_files
+            // 
+            this.lv_opened_files.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.lv_opened_files.HideSelection = false;
+            this.lv_opened_files.Location = new System.Drawing.Point(0, 79);
+            this.lv_opened_files.Name = "lv_opened_files";
+            this.lv_opened_files.Size = new System.Drawing.Size(207, 217);
+            this.lv_opened_files.TabIndex = 1;
+            this.lv_opened_files.UseCompatibleStateImageBehavior = false;
+            this.lv_opened_files.View = System.Windows.Forms.View.List;
+            this.lv_opened_files.Click += new System.EventHandler(this.lv_opened_files_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lbl_opened_files);
+            this.panel1.Controls.Add(this.btn_open_file);
+            this.panel1.Controls.Add(this.lv_opened_files);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(207, 296);
+            this.panel1.TabIndex = 2;
+            // 
+            // tb_main_text
+            // 
+            this.tb_main_text.Location = new System.Drawing.Point(226, 12);
+            this.tb_main_text.Multiline = true;
+            this.tb_main_text.Name = "tb_main_text";
+            this.tb_main_text.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tb_main_text.Size = new System.Drawing.Size(509, 296);
+            this.tb_main_text.TabIndex = 3;
+            // 
+            // lbl_opened_files
+            // 
+            this.lbl_opened_files.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_opened_files.Location = new System.Drawing.Point(3, 53);
+            this.lbl_opened_files.Name = "lbl_opened_files";
+            this.lbl_opened_files.Size = new System.Drawing.Size(201, 23);
+            this.lbl_opened_files.TabIndex = 2;
+            this.lbl_opened_files.Text = "Opened Files List:";
+            this.lbl_opened_files.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(743, 316);
+            this.Controls.Add(this.tb_main_text);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_open_file;
+        private System.Windows.Forms.ListView lv_opened_files;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox tb_main_text;
+        private System.Windows.Forms.Label lbl_opened_files;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
     }
 }
 
